366ecd4288294bcba06dd1067e97c06dd691c7a4
Pedro Maia <79416679+PedroHenrique-Maia@users.noreply.github.com>
2022-12-05 23:07:47 -0300

    Update TorneioTest.java

--- src/test/java/com/training/Tenis/Torneio/TorneioTest.java
+++ src/test/java/com/training/Tenis/Torneio/TorneioTest.java
@@ -8,7 +8,7 @@ public class TorneioTest {
 	@Test
 	public void testVitoria() {
 		TorneioTenis tenis = new TorneioTenis();
-		char[] Partidas = {'|wd\w};
+		char[] Partidas = {'V','V','P','P','P','V'};
 		int resp = tenis.Torneio(Partidas);
 		assertTrue(resp == 1);
 	}
